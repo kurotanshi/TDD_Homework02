05a44b60037aff2888b32dc3191599b81026c44e
Kuro Hsu <kurotanshi@gmail.com>
2016-07-12 13:31:43 +0800

    一二集各買了一本，第三集買了兩本，價格應為 100*3*0.9 + 100 = 370 元 [failed]

--- test.js
+++ test.js
@@ -96,4 +96,20 @@ suite('PotterShoppingCart', function(){
       assert.equal(expected, result);
     });
 
+
+    test('add_three_different_books_and_one_same_book_and_sum_should_be_370', function(){
+
+      // arrange
+      shoppingCart.push( new app.Book({ name: '哈利波特：神秘的魔法石', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：消失的密室', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：阿茲卡班的逃犯', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：阿茲卡班的逃犯', price: 100 }) );
+
+      // act
+      var expected = 370;
+      var result = app.DiscountCalculator( shoppingCart );
+
+      // assert
+      assert.equal(expected, result);
+    });
 });
