3743f86f53f57556f28a63172d97dd4670921c29
Kuro Hsu <kurotanshi@gmail.com>
2016-07-12 13:27:43 +0800

    一次買了整套，一二三四五集各買了一本，價格應為 100*5*0.75 = 375 元 [failed]

--- test.js
+++ test.js
@@ -61,6 +61,7 @@ suite('PotterShoppingCart', function(){
       assert.equal(expected, result);
     });
 
+
     test('add_four_different_books_and_sum_should_be_320', function(){
 
       // arrange
@@ -71,6 +72,24 @@ suite('PotterShoppingCart', function(){
 
       // act
       var expected = 320;
+      var result = app.DiscountCalculator( shoppingCart );
+
+      // assert
+      assert.equal(expected, result);
+    });
+
+
+    test('add_five_different_books_and_sum_should_be_375', function(){
+
+      // arrange
+      shoppingCart.push( new app.Book({ name: '哈利波特：神秘的魔法石', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：消失的密室', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：阿茲卡班的逃犯', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：火盃的考驗', price: 100 }) );
+      shoppingCart.push( new app.Book({ name: '哈利波特：鳳凰會的密令', price: 100 }) );
+
+      // act
+      var expected = 375;
       var result = app.DiscountCalculator( shoppingCart );
 
       // assert
